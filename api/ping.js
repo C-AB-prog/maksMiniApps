edef945b72701bf06d592a8c7a14e377bbb102fc
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-20 22:41:21 +0300

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,5 @@
+// CommonJS — совместим везде
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ ok: true, time: new Date().toISOString() });
+};
