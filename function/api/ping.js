41f303ea2b4bfb34fbfaae69107f9d785f6c9322
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-22 13:28:59 +0300

    Delete function/api directory

--- function/api/ping.js
+++
@@ -1,6 +0,0 @@
-// Cloudflare Pages Function: GET /api/ping
-export async function onRequest() {
-  return new Response(JSON.stringify({ ok:true, platform:'cloudflare-pages', time:new Date().toISOString() }), {
-    headers: { 'content-type':'application/json', 'access-control-allow-origin':'*' }
-  });
-}
