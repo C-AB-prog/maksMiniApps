122e20223e6900ec4b223629c19258902c3a9bdd
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-25 15:51:58 +0300

    Delete api directory

--- api/whoami.js
+++
@@ -1,8 +0,0 @@
-// /api/whoami.js
-import { requireUser } from './_utils/tg_node.js';
-
-export default async function handler(req, res) {
-  const user = await requireUser(req, res);
-  if (!user) return;
-  res.status(200).json({ ok: true, user });
-}
