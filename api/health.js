14e0233ffd00af65baa61f84b52c534d3cf0cdb1
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-25 16:48:21 +0300

    Create health.js

---
+++ api/health.js
@@ -0,0 +1,11 @@
+const { pool, ensureSchema } = require('./_db');
+module.exports = async (req, res) => {
+  try {
+    await ensureSchema();
+    await pool.query('SELECT 1');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).end(JSON.stringify({ ok: true }));
+  } catch (e) {
+    res.status(500).end(JSON.stringify({ ok:false, error: e.message }));
+  }
+};
