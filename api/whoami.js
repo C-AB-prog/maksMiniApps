60a6b7de027d1c8b011727b8230450b5659add02
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-26 23:05:04 +0300

    Delete api/whoami.js

--- api/whoami.js
+++
@@ -1,9 +0,0 @@
-// /api/whoami.js
-exports.config = { runtime: 'nodejs20.x' };
-
-const { getOrCreateUser, sendJSON } = require('./_utils');
-
-module.exports = async (req, res) => {
-  const { user, source } = getOrCreateUser(req, res);
-  return sendJSON(res, 200, { user, source });
-};
