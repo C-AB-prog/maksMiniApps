130bdfec63e6de3c84ffec6d5562b69b97ee978c
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-10-28 01:00:02 +0300

    Delete api/[...slug].js

--- api/[...slug].js
+++
@@ -1,5 +0,0 @@
-// api/[...slug].js
-import { json } from './_utils.js';
-export default function handler(req, res) {
-  json(res, 404, { ok:false, error:'Not found', path: req.url, method: req.method });
-}
